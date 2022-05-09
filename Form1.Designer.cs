03620d8192300459426dde31f72a0f358f12e5a6
liadk07 <91607695+liad07@users.noreply.github.com>
2022-05-09 21:31:42 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,185 @@
+﻿
+namespace scoreboard
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(328, 113);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(452, 358);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(1078, 113);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(402, 358);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Location = new System.Drawing.Point(448, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 72);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "אורחת";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 40F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(1175, 38);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 72);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "מארחת";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 89F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label3.Location = new System.Drawing.Point(797, 503);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(349, 158);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "00:00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 49F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label4.Location = new System.Drawing.Point(1372, 944);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(530, 88);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "אצטדיון סמי עופר";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1591, 637);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Segoe UI", 49F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label5.Location = new System.Drawing.Point(483, 526);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(73, 88);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Segoe UI", 49F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label6.Location = new System.Drawing.Point(1311, 526);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(73, 88);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Green;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "scoreboard";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+using System.Windows.Forms;
+
+namespace scoreboard
+{
+    public partial class Form1 : Form
+    {
+        System.Timers.Timer t;
+        int h, m, s, z;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            string home = Microsoft.VisualBasic.Interaction.InputBox("הכנס לינק ללוגו של המארחת",
+                       "Title",
+                       "link",
+                       0,
+                       0);
+            string homename = Microsoft.VisualBasic.Interaction.InputBox("הכנס שם של המארחת",
+                     "Title",
+                     "name",
+                     0,
+                     0);
+            string away = Microsoft.VisualBasic.Interaction.InputBox("הכנס לינק ללוגו של האורחת",
+            "Title",
+            "link",
+            0,
+            0);
+            string awayname = Microsoft.VisualBasic.Interaction.InputBox("הכנס שם של האורחת",
+         "Title",
+         "name",
+         0,
+         0);
+            string staduim = Microsoft.VisualBasic.Interaction.InputBox("הכנס שם איצטדיון",
+"Title",
+"staduim name",
+0,
+0);
+            string color = Microsoft.VisualBasic.Interaction.InputBox("הכנס צבע רקע",
+"Title",
+"color name",
+0,
+0);
+            h = 0;
+            z = 0;
+            //מארחת
+            pictureBox2.ImageLocation = home;
+            label2.Text = homename;
+            //אורחת
+            pictureBox1.ImageLocation = away;
+            label1.Text = awayname;
+
+            //איצטדיון
+            label4.Text = staduim;
+            //צבע רקע
+            this.BackColor = FromName(color);
+
+            //timer
+            t = new System.Timers.Timer();
+            t.Interval = 1000;
+            t.Elapsed += OnTimeEvent;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Hide();
+            t.Start();
+        
+        }
+
+        private void OnTimeEvent(object sender, ElapsedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                
+                s += 1;
+             
+                if (s == 60)
+                {
+                    s = 0;
+                    m += 1;
+                }
+                if (s <= 9)
+                {
+                    label3.Text = m.ToString() + ":0" + s.ToString();
+
+                }
+                if (m <= 9)
+                {
+                    label3.Text = "0" + m.ToString() + ":" + s.ToString();
+
+                }
+                if (s <= 9 && m <= 9)
+                {
+                    label3.Text = "0" + m.ToString() + ":0" + s.ToString();
+
+                }
+
+                if (s > 9 && m > 9)
+                {
+                    label3.Text = m.ToString() + ":" + s.ToString();
+
+                }
+                if (m == 45)
+                {
+
+                    label3.Text = "מחצית";
+
+
+                }
+
+                if (m == 90)
+  {
+      label3.Text = "game over";
+                    t.Stop();
+                }
+            }));
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Insert)
+            {
+                label5.Text = h++.ToString();
+            }
+            if (e.KeyCode == Keys.CapsLock)
+            {
+                label6.Text = z++.ToString();
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+        }
+        public static Color FromName(string name)
+        {
+            Color c = Color.FromName(name);
+
+            return c;
+        }
+
+    }
+}
